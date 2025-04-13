d3941a08cfe9d2a95b581cb6b3673fd3c433edb3
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-13 18:39:06 +0700

    test: add tests for inspect command

--- repl_test.go
+++ repl_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/phnthnhnm/go-pokedex/internal/api"
 )
 
 func TestCleanInput(t *testing.T) {
@@ -63,5 +65,45 @@ func TestCommandExplore(t *testing.T) {
 	err := commandExplore(cfg, args)
 	if err != nil {
 		t.Errorf("commandExplore returned an error: %v", err)
+	}
+}
+
+func TestCommandInspect(t *testing.T) {
+	cfg := &config{
+		Pokedex: map[string]api.Pokemon{
+			"pikachu": {
+				Name:   "pikachu",
+				Height: 4,
+				Weight: 60,
+				Stats: []struct {
+					BaseStat int `json:"base_stat"`
+					Stat     struct {
+						Name string `json:"name"`
+					} `json:"stat"`
+				}{
+					{BaseStat: 35, Stat: struct {
+						Name string `json:"name"`
+					}(struct{ Name string }{Name: "hp"})},
+					{BaseStat: 55, Stat: struct {
+						Name string `json:"name"`
+					}(struct{ Name string }{Name: "attack"})},
+				},
+				Types: []struct {
+					Type struct {
+						Name string `json:"name"`
+					} `json:"type"`
+				}{
+					{Type: struct {
+						Name string `json:"name"`
+					}(struct{ Name string }{Name: "electric"})},
+				},
+			},
+		},
+	}
+	args := []string{"pikachu"}
+
+	err := commandInspect(cfg, args)
+	if err != nil {
+		t.Errorf("commandInspect returned an error: %v", err)
 	}
 }
