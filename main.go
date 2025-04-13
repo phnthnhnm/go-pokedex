a01b4ebaeabcfef15e04738bd78a70d2e64acb8f
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-13 16:19:33 +0700

    feat: add map command

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -10,7 +13,20 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	Next     string
+	Previous string
+}
+
+type locationAreaResponse struct {
+	Results []struct {
+		Name string `json:"name"`
+	} `json:"results"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
 }
 
 func getCommands() map[string]cliCommand {
@@ -25,11 +41,19 @@ func getCommands() map[string]cliCommand {
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays location areas in the Pokemon world",
+			callback:    commandMap,
+		},
 	}
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	cfg := &config{
+		Next: "https://pokeapi.co/api/v2/location-area/",
+	}
 	for {
 		fmt.Print("Pokedex > ")
 		if !scanner.Scan() {
@@ -40,7 +64,7 @@ func main() {
 		if len(words) > 0 {
 			commandName := words[0]
 			if command, found := getCommands()[commandName]; found {
-				if err := command.callback(); err != nil {
+				if err := command.callback(cfg); err != nil {
 					fmt.Printf("Error: %s\n", err)
 				}
 			} else {
@@ -58,18 +82,53 @@ func cleanInput(text string) []string {
 	return words
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for _, cmd := range getCommands() {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
+	return nil
+}
+
+func commandMap(cfg *config) error {
+	if cfg.Next == "" {
+		fmt.Println("No more locations to display.")
+		return nil
+	}
+
+	resp, err := http.Get(cfg.Next)
+	if err != nil {
+		return fmt.Errorf("failed to fetch location areas: %w", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("received non-200 response: %d", resp.StatusCode)
+	}
+
+	var data locationAreaResponse
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	for _, location := range data.Results {
+		fmt.Println(location.Name)
+	}
+
+	cfg.Next = data.Next
+	cfg.Previous = data.Previous
 	return nil
 }
